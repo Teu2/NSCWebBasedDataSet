e3d24437b72cd495dfd8993a5b9c76285236ef55
KimTeh <89907829+KimTeh@users.noreply.github.com>
2022-09-26 17:41:55 +1000

    Update index.php to include index.js and tablesearch.js

--- NSC Labs (PHP)/index.php
+++ NSC Labs (PHP)/index.php
@@ -125,7 +125,7 @@
                 <div id="pagination-wrapper"></div>
             </div>
 
-            <!-- <script src="./scripts/tablesearch.js"></script> -->
+            <script src="./scripts/tablesearch.js"></script>
         </section> <!-- end of heading section-->
 
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
@@ -133,6 +133,6 @@
             AOS.init();
         </script>
 
-        <!-- <script src="./scripts/index.js"></script> -->
+        <script src="./scripts/index.js"></script>
     </body>
 </html>
