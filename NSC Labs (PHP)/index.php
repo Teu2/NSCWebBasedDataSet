6914ed6e2f7b7e1cb4568e56808de9ead95ec8f4
Dominic K Yeoh <89763540+Teu2@users.noreply.github.com>
2022-09-27 18:32:24 +1000

    minor fix to index.php

--- NSC Labs (PHP)/index.php
+++ NSC Labs (PHP)/index.php
@@ -7,10 +7,8 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-	<script src="./scripts/index.js"></script>
-	<script src="./scripts/tablesearch.js"></script>
     <link rel="icon" type="image/png" href="images/DataSetWindowsIcon.png">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="./styles/styles.css">
 </head>
     <body>
         <!-- side menu section -->
