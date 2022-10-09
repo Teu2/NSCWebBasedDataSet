bfd91ad8d0c4dd6c63ab14a80a296cd46cafe31c
Dominic K Yeoh <89763540+Teu2@users.noreply.github.com>
2022-10-10 00:28:15 +1100

    Sort function for dates

--- NSC Labs (PHP)/index.php
+++ NSC Labs (PHP)/index.php
@@ -49,27 +49,24 @@
                     <form action="" method="GET">
                         <div class="left_search">
                             <img src="images/search.png" alt="">
-                            <input type="text" placeholder="Search bugs..." size="60" id="myInput" name="search" value="<?php if(isset($_GET['search'])){
-                                    echo $_GET['search'];
-                                } ?>">
+                            <input type="text" placeholder="Search bugs..." size="60" id="myInput" name="search" value="<?php if(isset($_GET['search'])){echo $_GET['search'];} ?>">
                             <!-- <button type="submit" id="search_button" value="">Search</button> -->
                         </div>
                     </form>
                 </div>
 
-                <?php 
-                    
-                ?>
-
                 <!-- drop down box -->
                 <div class="right_sort" data-aos="fade-in" data-aos-easing="ease-in-out" data-aos-duration="700" data-aos-delay="400">
                     <div include="form-input-select()" class="select_box">
-                        <select required><option value="" hidden>Sort by... </option>
-                          <option value="1">Newest</option>
-                          <option value="2">Oldest</option>
-                          <option value="3">Type</option>
-                          <option value="4">Regression</option>
-                        </select>
+                        <form action="" method="GET">
+                            <select name="sorted" onchange="this.form.submit()">
+                                <option value="" hidden>Sort by... </option>
+                                <option value="reportDateNew" <?php if(isset($_GET['sorted'])) {echo $_GET['sorted'];} ?> >Report Date (Newest)</option>
+                                <option value="reportDateOld" <?php if(isset($_GET['sorted'])) {echo $_GET['sorted'];} ?> >Report Date (Oldest)</option>
+                                <option value="fixDateNew" <?php if(isset($_GET['sorted'])) {echo $_GET['sorted'];} ?> >Fix Date (Newest)</option>
+                                <option value="fixDateOld" <?php if(isset($_GET['sorted'])) {echo $_GET['sorted'];} ?> >Fix Date (Oldest)</option>
+                            </select>
+                        </form>
                       </div>
                 </div>
             </div>
@@ -96,7 +93,7 @@
                             $username = "epiz_32617535"; //change credentials to infinity free login for cpanel epiz_32617535
                             $password = "o7NWrwBIBBjPs"; //change credentials to infinity free login for cpanel o7NWrwBIBBjPs
                             $database = "epiz_32617535_master2"; // IMPORTANT!!!!!!!!!!!!!!!!!!!!!!! change nscbugdataset to the database name on your machine, change to epiz_32617535_master2
-                            
+
                             // create a connection
                             $connection = new mysqli($servername, $username, $password, $database);
 
@@ -115,7 +112,8 @@
                                 $filteredValues = cleanSearchString($searchedItem);
                                 $filteredValues = str_replace(" ","", $filteredValues);
 
-                                switch (true) {
+                                switch (true) { 
+
                                     case stristr($searchedItem, 'report') || stristr($searchedItem, 'Report'):
                                         $query = "SELECT * FROM `master2` WHERE CONCAT(reportDate) LIKE '%$filteredValues%'";
                                         runQuery($connection, $query);
@@ -128,24 +126,24 @@
 
                                     case searchIsDate($searchedItem): 
                                         $query = "SELECT * FROM `master2` WHERE CONCAT(reportDate, fixingDate) LIKE '%$filteredValues%'";
-                                        runQuery($connection, $query); break;
+                                        runQuery($connection, $query);
+                                        break;
 
                                     case  is_numeric($searchedItem): 
                                         //echo "<br/><b>string contains: </b> <br/>".$searchedItem;
                                         $query = "SELECT * FROM `master2` WHERE CONCAT(_record_number) LIKE '%$filteredValues%'";
                                         runQuery($connection, $query); 
                                         break;
-                                        
+
                                     case stristr($searchedItem, 'security') || stristr($searchedItem, 'bug-security'): 
-                                        $filteredValues = str_replace("bug","", $filteredValues);
-                                        $filteredValues = str_replace("Bug","", $filteredValues);  
+                                        $filteredValues = str_replace( array("bug", "Bug"), "", $filteredValues);
                                         $query = "SELECT * FROM `master2` WHERE CONCAT(bugType) LIKE '%$filteredValues%'";
                                         runQuery($connection, $query);
                                         break;
 
-                                    case stristr($searchedItem, 'bug'): 
-                                        //$query = "SELECT * FROM `master2` WHERE CONCAT(bugType) LIKE '%$filteredValues%'";
-                                        $query = "SELECT bugType FROM `master2` WHERE `bugType` LIKE `Bug`";
+                                    case stristr($searchedItem, 'bug') || stristr($searchedItem, 'Bug'): 
+                                        $filteredValues = str_replace("+","", $filteredValues);
+                                        $query = "SELECT * FROM `master2` WHERE binary bugType = `%$filteredValues%`";
                                         runQuery($connection, $query);
                                         break;
 
@@ -164,13 +162,18 @@
                                     default: echo "<tr><td colspan='9'>No Records Found</td></tr>"; break;
                                 }
 
+                            } else if (isset($_GET['sorted'])){
+                                sortData($connection);
+
                             } else {
                                 $sqlQuery  = "SELECT * FROM `master2`"; // IMPORTANT!!!!!!!!!!!!!!!!!!!!!!! change master to table name on your machine
                                 $result = $connection->query($sqlQuery);
                                 createTable($result);
                             }
+
                         ?>
                         <?php
+
                             function createTable($resultRows){
                                 $download = "DOWNLOAD";
                                 $link = "LINK";
@@ -183,7 +186,7 @@
                                     <td class='link'><a href='". $row["bugCommit"] ."' target='blank'>" . $link .  "<a></td>
                                     <td class='link'><a href='". $row["bugFixingCommit"] ."' target='blank'>" . $link .  "<a></td>";
                                     
-                                    if ($row["regressedOrNot"] === "Not") {
+                                    if ($row["regressedOrNot"] === "Not" || $row["regressedOrNot"] === "not" ) {
                                         echo "<td> Not </td>";
                                     } else {
                                         echo "<td class='link'><a href='". $row["regressedOrNot"] ."' target='blank'>" . $link .  "<a></td>";
@@ -194,6 +197,8 @@
                                     <td>" . $row["fixingDate"] . "</td>
                                     ";
                                 }
+
+                                // echo "<br/><b>query: </b> <br/>".$query;
                             }
 
                             function cleanSearchString($str){
@@ -216,6 +221,29 @@
                             function searchIsDate($str){
                                 if (substr_count($str, '-') > 1){
                                     return true;
+                                }
+                            }
+
+                            function sortData($connection){ 
+                                if(isset($_GET['sorted'])){
+    
+                                    switch ($_GET['sorted']) {
+                                        case "reportDateNew": $query = "SELECT * FROM `master2` ORDER BY reportDate desc"; 
+                                            runQuery($connection, $query);
+                                            break;
+                                            
+                                        case "reportDateOld": $query = "SELECT * FROM `master2` ORDER BY reportDate asc"; 
+                                            runQuery($connection, $query);
+                                            break;
+
+                                        case "fixDateNew": 
+                                            $query = "SELECT * FROM `master2` ORDER BY fixingDate desc"; 
+                                            runQuery($connection, $query); break;
+
+                                        case "fixDateOld": 
+                                            $query = "SELECT * FROM `master2` ORDER BY fixingDate asc"; 
+                                            runQuery($connection, $query); break;
+                                    }
                                 }
                             }
                         ?>
